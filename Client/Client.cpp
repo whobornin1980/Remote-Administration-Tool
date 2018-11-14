ad90bd93a49181ddc8a09701863a0cce4ef9726d
Jens Lindström <02jli01@skola.falun.se>
2018-11-14 22:39:40 +0100

    Added nice comments lul

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	client default_client("472jens.zapto.org", 1337);
 	default_client.connect_socket();
-	while (default_client.get_alive()) {
+	while (default_client.get_alive()) { // recv_data recurv calls send_data that recurv calls recv data lmao
 		cout << default_client.get_alive() << endl;
 		string output = "commit plz";
 		default_client.recv_data(output);
